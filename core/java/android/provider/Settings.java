6a939062177208de24bf2e86ca8fae5eefd1106c
Marko Man <darkobas@gmail.com>
2020-03-10 18:34:12 +0000

    SystemUI: FPS Info Overlay & Tile
    [neobuddy89]: Adapted tile for A13.
    
    Change-Id: Ie5322a8765d52b697cbfd53ed3852d036821fba5

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11031,6 +11031,13 @@ public final class Settings {
          */
         public static final String ADAPTIVE_CONNECTIVITY_ENABLED = "adaptive_connectivity_enabled";
 
+
+        /**
+         * Control whether the process FPS info meter should be shown.
+         * @hide
+         */
+        public static final String SHOW_FPS_OVERLAY = "show_fps_overlay";
+        
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
